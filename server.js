652cf7f6fdf00560e3c65bd605193668d3d61c09
fouaddev <fouaddev@bu.edu>
2019-10-27 16:37:55 -0400

    first angular deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/spotifly-app'));
+app.use(express.static(__dirname + '/dist/spotifly'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/spotifly-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/spotifly/index.html'));
 });
 
 // default Heroku PORT
